c29f9e64532db38dd62f1b8374fad37361e5dd21
liuyang-z <liuyang.z@hooowl.com>
2021-04-15 07:54:55 +0800

    [update] test freertos func

--- Src/user/FreeRTOS.c
+++ Src/user/FreeRTOS.c
@@ -3,57 +3,115 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-
-void vApplicationMallocFailedHook( void )
-{
-	/* vApplicationMallocFailedHook() will only be called if
-	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
-	function that will get called if a call to pvPortMalloc() fails.
-	pvPortMalloc() is called internally by the kernel whenever a task, queue,
-	timer or semaphore is created.  It is also called by various parts of the
-	demo application.  If heap_1.c or heap_2.c are used, then the size of the
-	heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
-	FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
-	to query the size of free heap space that remains (although it does not
-	provide information on how the remaining heap might be fragmented). */
-	taskDISABLE_INTERRUPTS();
-	for( ;; );
-}
+#if ( configUSE_MALLOC_FAILED_HOOK == 1 )
+	void vApplicationMallocFailedHook( void )
+	{
+		/* vApplicationMallocFailedHook() will only be called if
+		configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+		function that will get called if a call to pvPortMalloc() fails.
+		pvPortMalloc() is called internally by the kernel whenever a task, queue,
+		timer or semaphore is created.  It is also called by various parts of the
+		demo application.  If heap_1.c or heap_2.c are used, then the size of the
+		heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+		FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+		to query the size of free heap space that remains (although it does not
+		provide information on how the remaining heap might be fragmented). */
+		taskDISABLE_INTERRUPTS();
+		for( ;; );
+	}
+#endif
 /*-----------------------------------------------------------*/
 
-void vApplicationIdleHook( void )
-{
-	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
-	to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
-	task.  It is essential that code added to this hook function never attempts
-	to block in any way (for example, call xQueueReceive() with a block time
-	specified, or call vTaskDelay()).  If the application makes use of the
-	vTaskDelete() API function (as this demo application does) then it is also
-	important that vApplicationIdleHook() is permitted to return to its calling
-	function, because it is the responsibility of the idle task to clean up
-	memory allocated by the kernel to any task that has since been deleted. */
-}
+#if ( configUSE_IDLE_HOOK == 1 )
+	void vApplicationIdleHook( void )
+	{
+		/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+		to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+		task.  It is essential that code added to this hook function never attempts
+		to block in any way (for example, call xQueueReceive() with a block time
+		specified, or call vTaskDelay()).  If the application makes use of the
+		vTaskDelete() API function (as this demo application does) then it is also
+		important that vApplicationIdleHook() is permitted to return to its calling
+		function, because it is the responsibility of the idle task to clean up
+		memory allocated by the kernel to any task that has since been deleted. */
+	}
+#endif
 /*-----------------------------------------------------------*/
 
-void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
-{
-	( void ) pcTaskName;
-	( void ) pxTask;
+#if ( configCHECK_FOR_STACK_OVERFLOW > 0 )
+	void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+	{
+		( void ) pcTaskName;
+		( void ) pxTask;
 
-	/* Run time stack overflow checking is performed if
-	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
-	function is called if a stack overflow is detected. */
-	taskDISABLE_INTERRUPTS();
-	for( ;; );
-}
+		/* Run time stack overflow checking is performed if
+		configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+		function is called if a stack overflow is detected. */
+		taskDISABLE_INTERRUPTS();
+		for( ;; );
+	}
+#endif
 /*-----------------------------------------------------------*/
 
-void vApplicationTickHook( void )
-{
-	/* This function will be called by each tick interrupt if
-	configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h.  User code can be
-	added here, but the tick hook is called from an interrupt context, so
-	code must not attempt to block, and only the interrupt safe FreeRTOS API
-	functions can be used (those that end in FromISR()). */
-}
+#if  (  configUSE_TICK_HOOK > 0 )
+	void vApplicationTickHook( void )
+	{
+		/* This function will be called by each tick interrupt if
+		configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h.  User code can be
+		added here, but the tick hook is called from an interrupt context, so
+		code must not attempt to block, and only the interrupt safe FreeRTOS API
+		functions can be used (those that end in FromISR()). */
+	}
+#endif
 /*-----------------------------------------------------------*/
+
+#if ( configSUPPORT_STATIC_ALLOCATION == 1 )
+    /**
+     * task.h
+     * <pre>void vApplicationGetIdleTaskMemory( StaticTask_t ** ppxIdleTaskTCBBuffer, StackType_t ** ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize ) </pre>
+     *
+     * This function is used to provide a statically allocated block of memory to FreeRTOS to hold the Idle Task TCB.  This function is required when
+     * configSUPPORT_STATIC_ALLOCATION is set.  For more information see this URI: https://www.FreeRTOS.org/a00110.html#configSUPPORT_STATIC_ALLOCATION
+     *
+     * @param ppxIdleTaskTCBBuffer A handle to a statically allocated TCB buffer
+     * @param ppxIdleTaskStackBuffer A handle to a statically allocated Stack buffer for thie idle task
+     * @param pulIdleTaskStackSize A pointer to the number of elements that will fit in the allocated stack buffer
+     */
+    void vApplicationGetIdleTaskMemory( StaticTask_t ** ppxIdleTaskTCBBuffer,
+										StackType_t ** ppxIdleTaskStackBuffer,
+										uint32_t * pulIdleTaskStackSize ) {
+		static StaticTask_t idle_task_tcb;
+		static StackType_t	idle_statck_buffer[configMINIMAL_STACK_SIZE];
+
+		*ppxIdleTaskTCBBuffer   = &idle_task_tcb;
+		*ppxIdleTaskStackBuffer = idle_statck_buffer;
+		*pulIdleTaskStackSize   = configMINIMAL_STACK_SIZE;
+	}
+#endif
+
+#if ( (configSUPPORT_STATIC_ALLOCATION == 1) && (configUSE_TIMERS == 1) )
+
+    /**
+     * task.h
+     * <pre>void vApplicationGetTimerTaskMemory( StaticTask_t ** ppxTimerTaskTCBBuffer, StackType_t ** ppxTimerTaskStackBuffer, uint32_t *pulTimerTaskStackSize ) </pre>
+     * 
+     * This function is used to provide a statically allocated block of memory to FreeRTOS to hold the Timer Task TCB.  This function is required when 
+     * configSUPPORT_STATIC_ALLOCATION is set.  For more information see this URI: https://www.FreeRTOS.org/a00110.html#configSUPPORT_STATIC_ALLOCATION
+     * 
+     * @param ppxTimerTaskTCBBuffer   A handle to a statically allocated TCB buffer
+     * @param ppxTimerTaskStackBuffer A handle to a statically allocated Stack buffer for thie idle task
+     * @param pulTimerTaskStackSize   A pointer to the number of elements that will fit in the allocated stack buffer
+     */
+    void vApplicationGetTimerTaskMemory( StaticTask_t ** ppxTimerTaskTCBBuffer,
+                                         StackType_t ** ppxTimerTaskStackBuffer,
+                                         uint32_t * pulTimerTaskStackSize ) {
+		static StaticTask_t tmr_task_tcb;
+		static StackType_t	tmr_statck_buffer[configMINIMAL_STACK_SIZE];
+
+		*ppxTimerTaskTCBBuffer   = &tmr_task_tcb;
+		*ppxTimerTaskStackBuffer = tmr_statck_buffer;
+		*pulTimerTaskStackSize   = configMINIMAL_STACK_SIZE;
+	}
+
+#endif
+
--- Src/user/user_main.c
+++ Src/user/user_main.c
@@ -3,6 +3,10 @@
 #include "driver/gpio/led.h"
 
 
+#include "FreeRTOS.h"
+#include "task.h"
+#include "../driver/gpio/led.h"
+
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -15,121 +19,61 @@ void Error_Handler(void)
 }
 
 
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType        = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState              = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState          = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource         = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM              = 25;
-  RCC_OscInitStruct.PLL.PLLN              = 336;
-  RCC_OscInitStruct.PLL.PLLP              = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ              = 7;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource    = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider   = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider  = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider  = RCC_HCLK_DIV2;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
-    Error_Handler();
-  }
-}
-
-#ifdef  USE_FULL_ASSERT
-
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
-
-#include "FreeRTOS.h"
-#include "task.h"
-#include "../driver/gpio/led.h"
-
 void task_code(void *arg) {
 
-  led_cfg();
+    led_cfg();
 
-  float tmath = 0.1;
+    float a=2.1,b;
 
-  for(;;) {
-    vTaskDelay(300);
+	a=2.3*3.1;
+	b=a*12;
+	a=b/3.3;
 
-    tmath += 1.1;
 
-    if(led_stat(0)) {
-      led_act(0, 1);
-    } else {
-      led_act(0, 0);
+    for ( ;; ) {
+        vTaskDelay(100);
+
+        if(led_stat(0)) {
+        led_act(0, 1);
+        } else {
+        led_act(0, 0);
+        }
+        LOGI("hello freertos...");
     }
-    LOGI("hello freertos...");
-  }
 }
 
 void task_recv(void *arg) {
-  for(;;) {
-
-
-    vTaskDelay(100);
-  }
+    for ( ;; ) {
+        vTaskDelay(3000);
+    }
 }
 
 int main(void) {
 
-  TaskHandle_t task_c;
-  TaskHandle_t task_v;
+    TaskHandle_t task_c;
+    TaskHandle_t task_v;
 
-	HAL_Init();
-	SystemClock_Config();
+    HAL_Init();
 
-  xTaskCreate(
-    task_code,
-    "task code",
-    140,
-    NULL,
-    2,
-    &task_c
-  );
+    xTaskCreate(    task_code,
+                    "task code",
+                    140,
+                    NULL,
+                    2,
+                    &task_c );
 
-  xTaskCreate(
-    task_recv,
-    "task recv",
-    140,
-    NULL,
-    3,
-    &task_v
-  );
+    xTaskCreate(    task_recv,
+                    "task recv",
+                    140,
+                    NULL,
+                    3,
+                    &task_v );
 
-  vTaskStartScheduler();
+    vTaskStartScheduler();
 
-  for(;;) {
+    for ( ;; );
 
-  }
-
-	return 0;
+    return 0;
 }
 
 /* end of this file. */
