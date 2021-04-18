8a8d5559bdc9db61b4d4266e64708a95f37ccdca
liuyang-z <liuyang.z@hooowl.com>
2021-04-18 10:34:09 +0800

    [update] 调整 lib 目录结构

--- Src/lib/Segger_RTT/SEGGER_RTT_printf.c
+++ Src/lib/Segger_RTT/SEGGER_RTT_printf.c
@@ -52,8 +52,8 @@ Purpose : Replacement for printf to write formatted data via RTT
 Revision: $Rev: 17697 $
 ----------------------------------------------------------------------
 */
-#include "SEGGER_RTT.h"
-#include "SEGGER_RTT_Conf.h"
+#include "Segger_RTT/SEGGER_RTT.h"
+#include "Segger_RTT/SEGGER_RTT_Conf.h"
 
 /*********************************************************************
 *
