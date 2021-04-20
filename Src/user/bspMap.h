9eb8cce0973c31fef714155d9fcc626371f56f11
liuyang-z <liuyang.z@hooowl.com>
2021-04-20 16:21:43 +0800

    [fix] 修正内存动态分配的偏移地址及长度

--- Src/user/bspMap.h
+++ Src/user/bspMap.h
@@ -13,7 +13,9 @@
 
 #if ( VECT_TAB_SRAM )
 		// 在 RAM 中调试程序时，则 RAM 不能加入到动态内存分配
-		{ ( uint8_t * ) 0x20000000UL, 0x020000 },		/* RAM-Internal ( 128KB )	*/
+		// RAM-Internal(28KB + 100KB = 128KB) 
+		// 其中前 28KB 用于 bss 区域，后 100KB 用于动态内存分配
+		{ ( uint8_t * ) 0x20007000UL, 0x019000 },		/* RAM-Internal ( 100KB )	*/
 #endif
 
 		{ ( uint8_t * ) 0x68000000UL, 0x200000 },		/* SRAM ( 2MB)				*/
