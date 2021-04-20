8ebd9b0ea1da8529faaa510a86e064ff55ce4e3f
liuyang-z <liuyang.z@hooowl.com>
2021-04-20 16:01:44 +0800

    [update] 非 ram 调试时，自动将 ram 加入到动态分配

--- Src/user/bspMap.h
+++ Src/user/bspMap.h
@@ -9,8 +9,14 @@
 #if ( configUSE_CUSTUME_HEAP_TYPE == 5 )
 	HeapRegion_t xHeapRegions[] =
 	{
-		{ ( uint8_t * ) 0x10000000UL, 0x010000 },
-		{ ( uint8_t * ) 0x68000000UL, 0x200000 },
+		{ ( uint8_t * ) 0x10000000UL, 0x010000 },		/* CCRAM ( 64KB ) 			*/
+
+#if ( VECT_TAB_SRAM )
+		// 在 RAM 中调试程序时，则 RAM 不能加入到动态内存分配
+		{ ( uint8_t * ) 0x20000000UL, 0x020000 },		/* RAM-Internal ( 128KB )	*/
+#endif
+
+		{ ( uint8_t * ) 0x68000000UL, 0x200000 },		/* SRAM ( 2MB)				*/
 		{ NULL, 0 }
 	};
 #endif
