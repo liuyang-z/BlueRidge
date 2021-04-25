ab8a7163253ef4232cc91751a04a95777b438621
liuyang-z <liuyang.z@hooowl.com>
2021-04-25 14:23:46 +0800

    [other] 杂项更新

--- Src/user/bspMap.h
+++ Src/user/bspMap.h
@@ -11,9 +11,9 @@
 	{
 		{ ( uint8_t * ) 0x10000000UL, 0x010000 },		/* CCRAM ( 64KB ) 			*/
 
-#if ( VECT_TAB_SRAM )
+#if !defined ( VECT_TAB_SRAM )
 		// 在 RAM 中调试程序时，则 RAM 不能加入到动态内存分配
-		// RAM-Internal(28KB + 100KB = 128KB) 
+		// RAM-Internal(28KB + 100KB = 128KB)
 		// 其中前 28KB 用于 bss 区域，后 100KB 用于动态内存分配
 		{ ( uint8_t * ) 0x20007000UL, 0x019000 },		/* RAM-Internal ( 100KB )	*/
 #endif
