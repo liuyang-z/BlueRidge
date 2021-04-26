fba15be7409be02fb48ac3a1bd58233359042659
liuyang-z <liuyang.z@hooowl.com>
2021-04-26 10:34:57 +0800

    [update] 完善注释

--- Src/driver/tim/hal_tick.c
+++ Src/driver/tim/hal_tick.c
@@ -1,8 +1,11 @@
 
+/* Include ************************************************************************/
 #include "stm32f4xx.h"
 #include "stm32f4xx_hal_tim.h"
 #include "./hal_tick.h"
 
+
+/* struct pro *********************************************************************/
 typedef struct {
 	TIM_HandleTypeDef 	handle;
 	uint32_t 			priority;
@@ -11,6 +14,8 @@ typedef struct {
 	int8_t				status;
 } _hal_tick;
 
+
+/* Global defines *****************************************************************/
 _hal_tick _g_hal_tick = {
 
 #define HAL_TICK_IRQHandler		TIM7_IRQHandler
@@ -24,19 +29,27 @@ _hal_tick _g_hal_tick = {
 	.status = 0
 };
 
+
+/* HAL_TICK_IRQHandler *************************************************************
+*   TIM7 中断处理函数接口
+***********************************************************************************/
 void HAL_TICK_IRQHandler()
 {
 	HAL_TIM_IRQHandler(&_g_hal_tick.handle);
 }
 
-// called in irq handler
+/* tim_period_elapse ***************************************************************
+*   回调函数 called in irq handler
+***********************************************************************************/
 void tim_period_elapse(TIM_HandleTypeDef *htim)
 {
 	( void ) htim;
 	HAL_IncTick();
 }
 
-// called by HAL_TIM_Base_Init()
+/* tim_msp_init ********************************************************************
+*   回调函数 called by HAL_TIM_Base_Init()
+***********************************************************************************/
 void tim_msp_init(TIM_HandleTypeDef *htim)
 {
 	HAL_TICK_CLK_ENABLE();
@@ -45,6 +58,9 @@ void tim_msp_init(TIM_HandleTypeDef *htim)
 	HAL_NVIC_EnableIRQ(_g_hal_tick.inter_type);
 }
 
+/* HAL_InitTick ********************************************************************
+*   强引用接口，用于初始化 Hal Tick 定时器，此接口需要初始化系统时钟后调用
+***********************************************************************************/
 HAL_StatusTypeDef HAL_InitTick(uint32_t TickPriority)
 {
 	uint32_t tim_clock = 0;
